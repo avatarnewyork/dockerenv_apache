0f1f0ecbb126f5119e878eff677a9d3574a24b4f
patrick tully <patrick@avatarnewyork.com>
2014-11-24 10:00:27 -0500

    specifying exact node version 1.7.5

--- root/packages_nodejs.sh
+++ root/packages_nodejs.sh
@@ -2,6 +2,6 @@
 yum -y install npm
 
 # install node modules (globally)
-npm install -g uglify-js
-npm install -g uglifycss
-npm install -g less
+npm install -g uglify-js@2.3.6
+npm install -g uglifycss@0.0.10
+npm install -g less@1.7.5
