14e62e18feb750ff2b6e33165e7d8a39fe6c5d66
patrick tully <patrick@avatarnewyork.com>
2015-12-14 17:46:29 -0500

    added memcache, changed drush, removed extrainious xdebug

--- tests/PackageTest.php
+++ tests/PackageTest.php
@@ -6,15 +6,16 @@ class PackageTest extends PHPUnit_Framework_TestCase
   {
 
     error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
-    include 'PEAR/Registry.php';
+    
+    require_once "PEAR/Config.php";
+    $conf = new PEAR_Config();
+    $reg = $conf->getRegistry();
 
-    $reg = new PEAR_Registry;
     $packages = $reg->listPackages();
 
-    $this->assertContains('console_table', $packages);
+    //$this->assertContains('console_table', $packages);
     $this->assertContains('archive_tar', $packages);
-    $this->assertContains('xml_rpc', $packages);
-    $this->assertContains('console_getopt', $packages);
+    //$this->assertContains('console_getopt', $packages);
     $this->assertContains('xml_util', $packages);
     $this->assertContains('pear', $packages);
   }
