d5be9a6c1716082696f6998907860e75fec8fd82
patrick tully <patrick@avatarnewyork.com>
2016-02-09 09:52:36 -0500

    updating with drush 6.5 for compadibility with php5.3

--- root/packages_php.sh
+++ root/packages_php.sh
@@ -44,11 +44,12 @@ yes '' | pecl install -f solr
 
 echo "extension=solr.so" > /etc/php.d/solr.ini
 
-# Drush
-wget -O /usr/bin/drush http://files.drush.org/drush.phar; chmod 755 /usr/bin/drush
 
 # Composer
 cd /usr/bin; /usr/bin/php -dzend_extension=xdebug.so -r "readfile('https://getcomposer.org/installer');" | php
+
+# Drush
+COMPOSER_HOME=/.composer /usr/bin/php -dzend_extension=xdebug.so composer.phar global require drush/drush:6.5.0
 
 # PHPUnit
 COMPOSER_HOME=/.composer /usr/bin/php -dzend_extension=xdebug.so composer.phar global require --prefer-source "phpunit/phpunit=4.5.*"
